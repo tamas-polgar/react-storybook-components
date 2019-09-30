644d0f8b7b5b38efc30295c38f8d748addc41258
tamas-polgar <tamas.polgar.dev@gmail.com>
2019-09-30 17:39:34 -0300

    Implementing input components

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import Spinner from '../Spinner/Spinner';
+
+import './Input.css';
+
+const getClassNames = (props) => {
+  const classNames = ['input__field'];
+  const { icon, loading, valid, invalid } = props;
+
+  if (icon || loading) classNames.push('input__field--prepend');
+  if (valid) classNames.push('input__field--valid');
+  if (invalid) classNames.push('input__field--invalid');
+
+  return classNames.join(' ');
+}
 
 const Input = props => {
   const {
-    type, name, label, disabled, readonly, required, binding,
+    type,
+    name,
+    id,
+    value,
+    label,
+    disabled,
+    readonly,
+    required,
+    onChange,
+    caption,
+    icon,
+    maxLength,
+    loading,
   } = props;
-
-  const [value, setValue] = useState(binding || '');
 
   return (
     <div className="form-group">
@@ -14,14 +38,31 @@ const Input = props => {
       </label>
 
       <div className="form-group__content">
-        <input
-          type={type || 'text'}
-          disabled={disabled}
-          readonly={readonly}
-          required={required}
-          value={value}
-          onChange={({ target }) => setValue(target.value)}
-        /> 
+        <div className="input">
+          {
+            (icon || loading) &&
+            <div className="input__prepend">
+              {(icon && !loading) && <i className={`fa ${icon}`}></i>}
+              {loading && <Spinner/>}
+            </div>
+          }
+
+          <div className="input__container">
+            <input
+              className={getClassNames(props)}
+              type={type || 'text'}
+              name={name}
+              id={id}
+              value={value}
+              disabled={disabled || loading}
+              readonly={readonly}
+              required={required}
+              maxLength={maxLength}
+              onChange={onChange}
+            />
+            {caption && <span className="input__caption">{caption}</span>}
+          </div>
+        </div> 
       </div>
     </div>
   );  
--- src/stories/input.js
+++ src/stories/input.js
@@ -1,8 +1,106 @@
 import React, { useState } from 'react';
 import Input from '../components/Input/Input';
 
+const UseState = ({ render, initialValue }) => {
+  const [ variable, setVariable ] = useState(initialValue)
+  return render(variable, setVariable)
+}
+
 export function getInputVariants() {
   return <div>
-    <Input label="Default" required/> &nbsp;
+    <div style={{ maxWidth: 350 }}>
+      <UseState
+        initialValue="Default input"
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="Default"
+              value={value}
+              onChange={({ target }) => setValue(target.value)}
+            />
+          </div>
+        }}
+      />
+    </div>
+
+    <div style={{ marginTop: 15, maxWidth: 350 }}>
+      <UseState
+        initialValue=""
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="Required"
+              value={value}
+              onChange={({ target }) => setValue(target.value)}
+              required
+            />
+          </div>
+        }}
+      />
+    </div>
+
+    <div style={{ marginTop: 15, maxWidth: 350 }}>
+      <UseState
+        initialValue=""
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="With caption"
+              value={value}
+              caption="Caption content"
+              onChange={({ target }) => setValue(target.value)}
+            />
+          </div>
+        }}
+      />
+    </div>
+
+    <div style={{ marginTop: 15, maxWidth: 350 }}>
+      <UseState
+        initialValue=""
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="With icon"
+              value={value}
+              icon="fa-envelope"
+              onChange={({ target }) => setValue(target.value)}
+            />
+          </div>
+        }}
+      />
+    </div>
+
+    <div style={{ marginTop: 15, maxWidth: 350 }}>
+      <UseState
+        initialValue="Loading"
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="Loading"
+              value={value}
+              loading
+              onChange={({ target }) => setValue(target.value)}
+            />
+          </div>
+        }}
+      />
+    </div>
+
+    <div style={{ marginTop: 15, maxWidth: 350 }}>
+      <UseState
+        initialValue="Disabled"
+        render={(value, setValue) => {
+          return <div>
+            <Input
+              label="Disabled"
+              value={value}
+              disabled
+              onChange={({ target }) => setValue(target.value)}
+            />
+          </div>
+        }}
+      />
+    </div>
   </div>
 }
